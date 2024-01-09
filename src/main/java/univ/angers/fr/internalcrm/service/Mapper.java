e60230683c4c65e584f1a8fad0c66cbd29f567d8
fereoljob <job.fereol@gmail.com>
2024-01-09 12:47:35 +0100

    refactor mapper

--- src/main/java/univ/angers/fr/internalcrm/service/Mapper.java
+++ src/main/java/univ/angers/fr/internalcrm/service/Mapper.java
@@ -6,7 +6,7 @@ import univ.angers.fr.internalcrm.domain.ModelTO;
 
 public class Mapper {
     public static ModelTO fromInternalLeadDtoToModelTo(InternalLeadDto obj){
-        String[] tab = obj.getName().split(";");
+        String[] tab = obj.getName().split(",");
         ModelTO lead = new ModelTO(tab[0],tab[1], obj.getAnnualRevenue(),
         obj.getPhone(), obj.getStreet(),obj.getPostalCode(),obj.getCity(), obj.getCountry(),
          LocalDateTime.parse(obj.getCreationDate()),obj.getCompany(),obj.getState());
